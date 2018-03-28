1bf3c81f2d0b63eecb316da1f7f7505567837d9f
virtualclone <q199493422@gmail.com>
2018-03-28 09:56:22 +0800

    Update

--- src/NEDtoENU.cpp
+++ src/NEDtoENU.cpp
@@ -1,13 +1,23 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <nav_msgs/Odometry.h>
 
 
 sensor_msgs::Imu imu_ENU, imu_NED;
-
+geometry_msgs::PoseWithCovarianceStamped svo_pose;
+nav_msgs::Odometry filter_pose;
 
 void imu_cb(const sensor_msgs::Imu::ConstPtr& msg){
   imu_NED = *msg;
+}
+
+void svo_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
+  svo_pose = *msg;
+}
+
+void filter_cb(const nav_msgs::Odometry::ConstPtr& msg){
+  filter_pose = *msg;
 }
 
 void NEDtoENU(const sensor_msgs::Imu NED, sensor_msgs::Imu *imu_ENU){
@@ -24,6 +34,10 @@ void NEDtoENU(const sensor_msgs::Imu NED, sensor_msgs::Imu *imu_ENU){
   imu_ENU->orientation.z = -NED.orientation.z;
   imu_ENU->orientation.w = NED.orientation.w;
 
+  imu_ENU->orientation_covariance = imu_NED.orientation_covariance;
+  imu_ENU->angular_velocity_covariance = imu_NED.angular_velocity_covariance;
+  imu_ENU->linear_acceleration_covariance = imu_NED.linear_acceleration_covariance;
+
 
 
 }
@@ -36,17 +50,21 @@ int main(int argc, char **argv)
   /* The topic which is needed to be subscribed depends on what imu you use, here we use the imu on Pixhawk. */
 
   ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("/mavros/imu/data", 10, imu_cb);
+  ros::Subscriber svo_sub = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/svo/pose_imu", 10, svo_cb);
+  ros::Subscriber filter_sub = nh.subscribe<nav_msgs::Odometry>("/odometry/filtered", 10, filter_cb);
   ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("/imu/enu",10);
 
   ros::Rate rate(50);
 
   imu_ENU.header.frame_id = "base_link";
-  imu_ENU.orientation_covariance = {1 , 0 , 0 , 0 ,1 , 0 , 0 ,0 , 1};
+
 
   while (ros::ok()) {
    imu_ENU.header.stamp = ros::Time::now();
    NEDtoENU(imu_NED, &imu_ENU);
    imu_pub.publish(imu_ENU);
+   ROS_INFO("SVO:    x = %f, y = %f, z = %f",svo_pose.pose.pose.position.x, svo_pose.pose.pose.position.y, svo_pose.pose.pose.position.z);
+   ROS_INFO("Filter: x = %f, y = %f, z = %f", filter_pose.pose.pose.position.x, filter_pose.pose.pose.position.y, filter_pose.pose.pose.position.z);
    ros::spinOnce();
    rate.sleep();
   }
